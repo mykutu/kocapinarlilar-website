c83ad265bf7e828dfd7b3721467691c42de0b4ad
mykutu <mykutu@hotmail.com>
2026-07-15 00:12:56 +0300

    Fix and harden legacy v1/ page
    - Fixed broken favicon reference (favicon.png doesn't exist, actual
      file is favicon.ico)
    - Fixed invalid CSS: #map_canvas was nested inside the body{} block,
      which is not valid CSS outside the newer native-nesting spec and
      caused the whole body rule (background image/color) to silently
      fail to parse in most browsers
    - jQuery 1.3.2 (2009, known XSS CVEs) loaded over http:// -> upgraded
      to the same pinned 1.10.2 + SRI already used on the main page, over
      https://
    - The Google Maps JavaScript API call used the long-removed `sensor`
      parameter and no API key, so it silently fails today (Google now
      requires a billed key for the JS API) - the "show map" feature was
      completely broken. Replaced with a key-free Maps iframe embed
      pointing at the same coordinates, preserving the same click-to-
      reveal behaviour without needing any credentials.
    - Removed dead Classic Google Analytics snippet (ga.js/_gaq was
      retired by Google years ago, the script did nothing)
    - Removed the Google+ link (service shut down in 2019), consistent
      with the same cleanup already done on the main page
    - Added a viewport meta tag for mobile compatibility

--- v1/index.php
+++ v1/index.php
@@ -2,10 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="robots" content="index, follow" />
 <meta name="keywords" content="gelinlik,damatlık,nişanlık,abiye,kocapınarlılar,balıkesir,izmir,ahmet kayan" />
 <meta name="description" content="Balıkesir'in En İyi Gelinlik Nişanlık Damatlık ve Abiye Mağazası. Bize uğramadan karar vermeyin." />
-<link href="favicon.png" rel="shortcut icon" type="image/x-icon" />
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
 <title>Kocapınarlılar Konfeksiyon Gelinlik Damatlık Nişanlık</title>
 
 <style type="text/css">
@@ -22,9 +23,8 @@ body {
 	background-repeat: no-repeat;
 	margin-left: 10px;
 	font:italic 16px/27px 'Times New Roman',Times,serif;
-
-#map_canvas { height: 100% }
 }
+#map_canvas { height: 100% }
 #sliderDiv {
     display: none;
 }
@@ -49,7 +49,7 @@ a:active {
 	color: #FFF;
 }
 </style>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" integrity="sha384-aBL3Lzi6c9LNDGvpHkZrrm3ZVsIwohDD7CDozL0pk8FwCrfmV7H9w8j3L7ikEv6h" crossorigin="anonymous" type="text/javascript"></script>
 <script type="text/javascript">
 //<![CDATA[
 function ShowHide(){
@@ -57,45 +57,16 @@ $("#sliderDiv").animate({"height": "toggle"}, { duration: 1000 });
 }
 //]]>
 </script>
-<script type="text/javascript"
-    src="http://maps.google.com/maps/api/js?sensor=true">
-</script>
-<script type="text/javascript">
-  function initialize() {
-    var latlng = new google.maps.LatLng(39.648345,27.883166);
-    var myOptions = {
-      zoom: 19,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        myOptions);
-  }
-</script>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-836566-11']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </head>
-<body onload="initialize()">
+<body>
 <div id="contact">
 <img src="logo.png" width="321" height="90" alt="Kocapınarlılar Konfeksiyon" />
 <p>Yaymacılar Caddesi No 33<br />
 10100 Balıkesir Türkiye</p>
 <p>+90 266 241 3699</p>
-<a href="https://plus.google.com/110506235829106323420" rel="publisher">Google+</a>
 <p><a onclick="ShowHide(); return false;" href="#">Haritada Göster</a></p>
 <div id="sliderDiv">
-	<div id="map_canvas" style="width:300px; height:200px"></div>
+	<iframe src="https://maps.google.com/maps?q=39.648345,27.883166&z=19&output=embed" width="300" height="200" style="border:0" loading="lazy" title="Kocapınarlılar Konfeksiyon Harita"></iframe>
 <h5>Pzt - Cmt : 09:00 - 19:00</h5>
 <a onclick="ShowHide(); return false;" href="#">Haritayı Gizle</a></div>
 </div>
